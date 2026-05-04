f0613f34a17d06d2c4029e8e7ab0c9f809ba724c
YonghoKwon <ho2708@naver.com>
2026-05-04 19:28:18 +0900

    fix: import sensitive log masker in HTTP logging filter

--- src/main/java/com/dt/digitaltwinsimulator/config/RequestResponseLoggingFilter.java
+++ src/main/java/com/dt/digitaltwinsimulator/config/RequestResponseLoggingFilter.java
@@ -1,5 +1,6 @@
 package com.dt.digitaltwinsimulator.config;
 
+import com.dt.digitaltwinsimulator.logic.SensitiveLogMasker;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
