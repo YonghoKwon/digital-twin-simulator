7cdaebc33db855e280d0748fef982b972dd79f96
YonghoKwon <ho2708@naver.com>
2026-05-04 11:01:40 +0900

    feat: add validation and explicit message count to file request dto

--- src/main/java/com/dt/digitaltwinsimulator/entity/dto/ActiveMQRequestFileDto.java
+++ src/main/java/com/dt/digitaltwinsimulator/entity/dto/ActiveMQRequestFileDto.java
@@ -1,5 +1,8 @@
 package com.dt.digitaltwinsimulator.entity.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,38 +11,60 @@ import lombok.ToString;
  * ActiveMQ Request File Dto
  *
  * <blockquote><pre>
- * activeMQIp : activeMQ ip
- * id : activeMQ id
- * pw : activeMQ password
- * topic : activeMQ topic
+ * activeMQIp : ActiveMQ ip
+ * id : ActiveMQ id
+ * pw : ActiveMQ password
+ * topic : ActiveMQ topic
  * tcName : transaction name
  *
  * delayTime : The interval between the previous message and the next message
  * repeatBoolean : repeat or not
- * repeatTime : the number of repetitions
+ * repeatTime : total duration in milliseconds when messageCount is not specified
+ * messageCount : explicit number of generated messages. Takes precedence over repeatTime.
  *
  * filePath : tc format & data file Path
  * fileName : tc format & data file Name
  *
- * concurrentUsers : Number of concurrent tasks for load testing
+ * concurrentTasks : Number of concurrent tasks for load testing
  * </pre></blockquote>
  */
 @ToString
 @Getter
 @Setter
 public class ActiveMQRequestFileDto {
+    @NotBlank
     private String activeMQIp;
+
+    @NotBlank
     private String id;
+
+    @NotBlank
     private String pw;
+
+    @NotBlank
     private String topic;
+
+    @NotBlank
     private String tcName;
 
+    @Min(0)
     private int delayTime;
+
     private boolean repeatBoolean;
+
+    @Min(0)
     private int repeatTime;
 
+    @Min(0)
+    private int messageCount;
+
+    @NotBlank
     private String filePath;
+
+    @NotBlank
     private String fileName;
 
+    @Min(1)
+    @Max(2000)
     private int concurrentTasks = 1;
 }
