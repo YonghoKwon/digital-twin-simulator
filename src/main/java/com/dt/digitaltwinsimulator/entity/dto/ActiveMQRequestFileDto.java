8afb2dc5850b9301fa3249657a908b61cecafb49
YonghoKwon <ho2708@naver.com>
2026-05-04 14:41:38 +0900

    feat: allow broker defaults for file requests

--- src/main/java/com/dt/digitaltwinsimulator/entity/dto/ActiveMQRequestFileDto.java
+++ src/main/java/com/dt/digitaltwinsimulator/entity/dto/ActiveMQRequestFileDto.java
@@ -7,41 +7,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-/**
- * ActiveMQ Request File Dto
- *
- * <blockquote><pre>
- * activeMQIp : ActiveMQ ip
- * id : ActiveMQ id
- * pw : ActiveMQ password
- * topic : ActiveMQ topic
- * tcName : transaction name
- *
- * delayTime : The interval between the previous message and the next message
- * repeatBoolean : repeat or not
- * repeatTime : total duration in milliseconds when messageCount is not specified
- * messageCount : explicit number of generated messages. Takes precedence over repeatTime.
- *
- * filePath : tc format & data file Path
- * fileName : tc format & data file Name
- *
- * concurrentTasks : Number of concurrent tasks for load testing
- * </pre></blockquote>
- */
 @ToString
 @Getter
 @Setter
 public class ActiveMQRequestFileDto {
-    @NotBlank
     private String activeMQIp;
-
-    @NotBlank
     private String id;
-
-    @NotBlank
     private String pw;
-
-    @NotBlank
     private String topic;
 
     @NotBlank
